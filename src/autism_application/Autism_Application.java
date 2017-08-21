e7f80bb594124f796de5a2add4fb548fd877a232
Alpha23 <myprojectallandall@gmail.com>
2017-08-21 14:47:07 +0530

    initial commit

---
+++ src/autism_application/Autism_Application.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package autism_application;
+
+import Window.Front_Page;
+
+/**
+ *
+ * @author HalfBlood
+ */
+public class Autism_Application {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Front_Page f = new Front_Page();
+        f.setVisible(true);
+    }
+    
+}
